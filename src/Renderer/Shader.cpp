8689ff4f0bf4fa882803bb4e3859693cab449038
LEE JAE HYUN <leejaehyun1223@gmail.com>
2021-06-05 17:12:53 +0900

    TODO: glm,glfw,Eigen eigen fetch

--- src/Renderer/Shader.cpp
+++ src/Renderer/Shader.cpp
@@ -10,6 +10,14 @@ void Shader::use() {
 
 Shader::Shader(std::string vtshader_path, std::string fgshader_path) {
 
+    vertex_shader_path=vtshader_path;
+    fragment_shader_path=fgshader_path;
+
+//    vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+//    fragment_shader_id=glCreateShader(GL_FRAGMENT_SHADER);
+
+
+
 }
 
 int Shader::getProgramID() {
