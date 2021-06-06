dee70e0ad241b47458482381ae76cca9aa861b8e
LEE JAE HYUN <leejaehyun1223@gmail.com>
2021-06-06 23:08:07 +0900

    TODO: glm,glfw,Eigen eigen fetch

--- src/sampleScene.cpp
+++ src/sampleScene.cpp
@@ -1,8 +1,76 @@
 #include <iostream>
+//#include <GL/glew.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Eigen/Dense>
+#include "Renderer/imgui/imgui.h"
+#include "Renderer/imgui/imgui_impl_opengl3.h"
+#include "Renderer/imgui/imgui_impl_glfw.h"
 
 
-// create sample scene
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+
+
+    MatrixXd m = MatrixXd::Random(3,3);
+    m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    cout << "m =" << endl << m << endl;
+    VectorXd v(3);
+    v << 1, 2, 3;
+    cout << "m * v =" << endl << m * v << endl;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    //if (glewInit() != GLEW_OK) { cout << "succeed\n"; }
+    /* Create a windowed mode window and its OpenGL context */
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        ImGui::Begin("Demo window");
+        ImGui::Button("Hello!");
+        ImGui::End();
+
+        // Render dear imgui into screen
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
