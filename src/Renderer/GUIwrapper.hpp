47de2e75b7c2c3b9ee702e7bdb7f051a54c4dfba
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2021-06-09 02:25:41 +0900

    DONE: assimp add TODO: renderer class compelete and unit test

---
+++ src/Renderer/GUIwrapper.hpp
@@ -0,0 +1,14 @@
+//
+// Created by 가디언 on 2021-06-09.
+//
+
+#ifndef LEEFRAMEWORK_GUIWRAPPER_HPP
+#define LEEFRAMEWORK_GUIWRAPPER_HPP
+
+
+class GUIwrapper {
+
+};
+
+
+#endif //LEEFRAMEWORK_GUIWRAPPER_HPP
--- src/utils/mathUtils.cpp
+++ src/utils/mathUtils.cpp
@@ -2,3 +2,11 @@
 // Created by test on 2021-06-04.
 //
 
+void glm2eigen(){
+
+}
+void eigen2glm(){
+
+}
+
+
