15b18d8cfb5c355d167501de513bf67983486be8
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2021-06-12 02:48:02 +0900

    lab_build_and_Renderer_

---
+++ src/Mesh/PrimitiveMesh.hpp
@@ -0,0 +1,18 @@
+//
+// Created by 가디언 on 2021-06-12.
+//
+
+#ifndef LEEFRAMEWORK_PRIMITIVEMESH_HPP
+#define LEEFRAMEWORK_PRIMITIVEMESH_HPP
+
+#include "Mesh.hpp"
+class PrimitiveMesh :Mesh{
+
+public:
+
+private:
+
+};
+
+
+#endif //LEEFRAMEWORK_PRIMITIVEMESH_HPP
